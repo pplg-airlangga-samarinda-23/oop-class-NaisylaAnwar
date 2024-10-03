d848ce5db327aadd7cf6e34d61ddd05f7d5d77c3
NaisylaAnwar <23_naisylanwar@student.smkti.net>
2024-10-03 22:07:44 +0700

    selesai

--- soal-1/Balok.php
+++ soal-1/Balok.php
@@ -1,24 +1,63 @@
 <?php
 class Balok
 {
+    private $p;
+    private $l;
+    private $t;
+
     // deklarasikan atribut-atribut kelas Balok
 
-    
+
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($p, $l, $t)
+    {
+        $this->p = $p;
+        $this->l = $l;
+        $this->t = $t;
+    }
 
+    public function setP($p)
+    {
+        $this->p = $p;
+    }
+    public function setL($l)
+    {
+        $this->l = $l;
+    }
+    public function setT($t)
+    {
+        $this->t = $t;
+    }
 
+    public function getP()
+    {
+        return $this->p;
+    }
+    public function getL()
+    {
+        return $this->l;
+    }
+    public function getT()
+    {
+        return $this->t;
+    }
     // definisikan metode menghitung volume, luas permukaan, 
+    public function hitungVolume()
+    {
+        return $this->p * $this->l * $this->t;
+    }
+
+    public function hitungLuas()
+    {
+        return 2 * ($this->p * $this->l + $this->p * $this->t + $this->l * $this->t);
+    }
 }
 
 $balok_1 = new Balok(4, 8, 5);
 $balok_2 = new Balok(3, 10, 12);
+echo "Volume: " . $balok_1->hitungVolume() . "<br>";
+echo "Luas Permukaan: " . $balok_1->hitungLuas() . "<br>";
 
-// contoh output: Balok
-
-// Balok 1
-// Volume: 160
-// Luas Permukaan: 184
-
-// Balok 2
-// Volume: 360
-// Luas Permukaan: 372
+echo "Volume: " . $balok_2->hitungVolume() . "<br>";
+echo "Luas Permukaan: " . $balok_2->hitungLuas() . "<br>";
+?>
--- soal-2/Siswa.php
+++ soal-2/Siswa.php
@@ -2,17 +2,61 @@
 
 class Siswa
 {
+    private $nama;
+    private $nisn;
+    private $alamat;
+    private $jurusan;
+    private $tanggalhr;
     // deklarasikan atribut-atribut kelas Siswa
 
     
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($nama, $nisn, $alamat, $jurusan, $tanggalhr)
+    {
+        $this->nama = $nama;
+        $this->nisn = $nisn;
+        $this->jurusan = $jurusan;
+        $this->tanggalhr = $tanggalhr;
+    }
 
+    public function setNama($nama)
+    {
+        $this->nama = $nama;
+    }
+    public function setNisn($nisn)
+    {
+        $this->nisn = $nisn;
+    }
+    public function setJurusan($jurusan)
+    {
+        $this->jurusan = $jurusan;
+    }
+    public function setTanggalhr($tanggalhr)
+    {
+        $this->tanggalhr = $tanggalhr;
+    }
 
+    public function getNama()
+    {
+        return $this->nama;
+    }
+    public function getNisn()
+    {
+        return $this->nisn;
+    }
+    public function getJurusan()
+    {
+        return $this->jurusan;
+    }
+    public function getTanggalhr()
+    {
+        return $this->tanggalhr;
+    }
     // buat metode untuk menghitung umur siswa
     public function hitungUmur() {
 
 
-        return $umur;
+        return ;
     }
 }
 
